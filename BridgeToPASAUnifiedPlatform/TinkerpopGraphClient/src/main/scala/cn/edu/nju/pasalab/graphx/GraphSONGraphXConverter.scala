8e5486d09d7c15d18e5debed5e6b970888ba5c05
lijunhong <394855878@qq.com>
2018-09-20 15:55:30 +0800

    add some annotation to the converter

--- BridgeToPASAUnifiedPlatform/TinkerpopGraphClient/src/main/scala/cn/edu/nju/pasalab/graphx/GraphSONGraphXConverter.scala
+++ BridgeToPASAUnifiedPlatform/TinkerpopGraphClient/src/main/scala/cn/edu/nju/pasalab/graphx/GraphSONGraphXConverter.scala
@@ -2,21 +2,20 @@ package cn.edu.nju.pasalab.graphx
 
 import java.util
 import java.lang
-import java.util.{HashMap, Map}
+import java.net.URI
+import java.util.HashMap
 
-import cn.edu.nju.pasalab.graph.impl.hadoopgraphcomputer.Common
-import cn.edu.nju.pasalab.graph.impl.hadoopgraphcomputer.Common.VertexDirection
-import cn.edu.nju.pasalab.graph.impl.util.HDFSUtils
-import org.apache.tinkerpop.gremlin.structure.util.star.StarGraph
 import org.apache.spark.{SparkContext, graphx}
 import org.apache.spark.rdd.RDD
 import com.google.common.base.Charsets
 import com.google.common.hash.Hashing
 import org.apache.commons.configuration.BaseConfiguration
-import org.apache.hadoop.fs.Path
-import org.apache.spark.api.java.{JavaPairRDD, JavaRDD, JavaSparkContext}
-import org.apache.spark.graphx.{EdgeDirection, VertexRDD}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.api.java.{JavaPairRDD, JavaSparkContext}
+import org.apache.spark.graphx.EdgeDirection
 import org.apache.tinkerpop.gremlin.structure.{Edge, Property, T, Vertex}
+import org.apache.tinkerpop.gremlin.structure.util.star.StarGraph
 import org.apache.tinkerpop.gremlin.hadoop.Constants
 import org.apache.tinkerpop.gremlin.hadoop.structure.HadoopGraph
 import org.apache.tinkerpop.gremlin.hadoop.structure.io.graphson.{GraphSONInputFormat, GraphSONOutputFormat}
@@ -28,7 +27,7 @@ import scala.collection.JavaConverters._
 
 
 
-class GraphSONGraphXConvertor extends Serializable {
+class GraphSONGraphXConverter extends Serializable {
 
   val DEFAULT_VERTEX_LABEL = "SimpleV"
   val DEFAULT_EDGE_LABEL = "SimpleE"
@@ -38,24 +37,28 @@ class GraphSONGraphXConvertor extends Serializable {
   }
 
   def hashEdgeID(src:String,dst:String,isOut:Boolean):Long = {
-    if(isOut){
+    if (isOut) {
       Math.abs(Hashing.sha256.hashString(src+dst,Charsets.UTF_8).asLong())
-    }else{
+    } else {
       Math.abs(Hashing.sha256.hashString(dst+src,Charsets.UTF_8).asLong())
     }
   }
 
   def getOrCreateVertexForStarGraph(graph:StarGraph, cache:util.HashMap[Long, Vertex],
-                      name: Long,isStar: Boolean,
+                      name: Long,isCenter: Boolean,
                       properties :util.HashMap[String, java.io.Serializable]):Vertex = {
 
-    if (cache.containsKey(name) && !isStar) cache.get(name)
-    else if (!cache.containsKey(name) && !isStar){
+    // Get the vertex contained in the cache or create one
+    // Return the vertex
+    if (cache.containsKey(name) && !isCenter) cache.get(name)
+    else if (!cache.containsKey(name) && !isCenter) {
       val v = graph.addVertex(T.id, name:lang.Long, T.label, DEFAULT_VERTEX_LABEL)
       cache.put(name, v)
       v
-    } else if (cache.containsKey(name) && isStar) {
+    } else if (cache.containsKey(name) && isCenter) {
       val v = cache.get(name)
+
+      // Add the properties only if the vertex is center vertex
       properties.asScala.foreach(pro => {
         v.property(pro._1, pro._2)
       })
@@ -82,29 +85,30 @@ class GraphSONGraphXConvertor extends Serializable {
   def fromGraphXToGraphSON(graphRDD: graphx.Graph[util.HashMap[String, java.io.Serializable],
                                                   util.HashMap[String, java.io.Serializable]],
                            outputFilePath:String):Unit = {
+    // Get the sparkcontext
     val sc = graphRDD.edges.sparkContext
 
-    //val vertexRDD : RDD[Vertex] = graphRDD.triplets.map(triple => {
-//      ((triple.srcId,triple.srcAttr),triple.dstId,triple.dstAttr,triple.attr)
-//}).groupBy(_._1).map(edgeTuples => {
-    //val vertexRDD: RDD[Vertex] = graphRDD.collectEdges(EdgeDirection.Out)
+    // Tuple2 of the src vertex id and the array of all its out Edge
     val vertexRDDWithEdgeProperties = graphRDD.collectEdges(EdgeDirection.Out)
 
+    // Join the vertex id ,vertex attribute and the array of all its out edges(as adjacent edges)
     val tinkerPopVertexRDD = graphRDD.outerJoinVertices(vertexRDDWithEdgeProperties) {
-      case (centerVertexName, centerVertexAttr, adjs) => {
-
+      case (centerVertexID, centerVertexAttr, adjs) => {
+        // Create the StarGraph and its center
         val graph = StarGraph.open
         val cache = new util.HashMap[Long, Vertex]
         val centerVertex:Vertex = getOrCreateVertexForStarGraph(graph,cache,
-                                                                centerVertexName,true,centerVertexAttr)
-        // Add adjacency edges
+                                                                centerVertexID,true,centerVertexAttr)
+        // Add adjacent edges
         adjs.get.map(edge => {
 
-          val anotherVertexName = edge.dstId
+          // Create the adjacent vertex
+          val anotherVertexID = edge.dstId
           val edgeProperties = edge.attr
           val srcV = centerVertex
-          val dstV :Vertex = getOrCreateVertexForStarGraph(graph,cache,anotherVertexName,false, null)
+          val dstV :Vertex = getOrCreateVertexForStarGraph(graph,cache,anotherVertexID,false, null)
 
+          // For both direction, add an edge between the both vertices
           val outedgeID:lang.Long = hashEdgeID(edge.srcId.toString,edge.dstId.toString,true)
           val outedge = srcV.addEdge(DEFAULT_EDGE_LABEL,dstV,T.id,outedgeID)
           if (outedge != null && edgeProperties.size > 0) addProperties(outedge, edgeProperties)
@@ -112,30 +116,32 @@ class GraphSONGraphXConvertor extends Serializable {
           val inedgeID:lang.Long = hashEdgeID(edge.srcId.toString,edge.dstId.toString,false)
           val inedge = dstV.addEdge(DEFAULT_EDGE_LABEL,srcV,T.id,inedgeID)
           if (inedge != null && edgeProperties.size > 0) addProperties(inedge, edgeProperties)
-
         })
 
+        // Return the center vertex
         graph.getStarVertex
       }
     }.vertices.map {case(vid, vertex) => vertex}
 
+    // Change the form for adapting to the java interface
     val tinkergraphRDD = tinkerPopVertexRDD.map(vertex => (AnyRef :AnyRef, new VertexWritable(vertex))).toJavaRDD()
 
 
     ///////// Output the VertexRDD
-
     val outputConf = new BaseConfiguration
     val tmpOutputPath = outputFilePath + "~"
+    val hadoopConf = new Configuration
+    val path = URI.create(outputFilePath)
     outputConf.setProperty(Constants.GREMLIN_HADOOP_GRAPH_WRITER, classOf[GraphSONOutputFormat].getName)
     outputConf.setProperty(Constants.GREMLIN_HADOOP_OUTPUT_LOCATION, tmpOutputPath)
-    HDFSUtils.getFS(outputFilePath).delete(new Path(tmpOutputPath), true)
-    HDFSUtils.getFS(outputFilePath).delete(new Path(outputFilePath), true)
-    HDFSUtils.getFS(outputFilePath).deleteOnExit(new Path(tmpOutputPath))
+    FileSystem.get(path,hadoopConf).delete(new Path(tmpOutputPath), true)
+    FileSystem.get(path,hadoopConf).delete(new Path(outputFilePath), true)
+    FileSystem.get(path,hadoopConf).deleteOnExit(new Path(tmpOutputPath))
     val formatRDD = new OutputFormatRDD
     formatRDD.writeGraphRDD(outputConf, JavaPairRDD.fromJavaRDD(tinkergraphRDD))
     sc.stop()
-    HDFSUtils.getFS(outputFilePath).rename(new Path(tmpOutputPath, "~g"), new Path(outputFilePath))
-    HDFSUtils.getFS(outputFilePath).delete(new Path(tmpOutputPath), true)
+    FileSystem.get(path,hadoopConf).rename(new Path(tmpOutputPath, "~g"), new Path(outputFilePath))
+    FileSystem.get(path,hadoopConf).delete(new Path(tmpOutputPath), true)
   }
 
 
@@ -148,30 +154,33 @@ class GraphSONGraphXConvertor extends Serializable {
     inputGraphConf.setProperty(Constants.GREMLIN_HADOOP_GRAPH_READER, classOf[GraphSONInputFormat].getName)
     inputGraphConf.setProperty(Constants.GREMLIN_HADOOP_INPUT_LOCATION, inputGraphFilePath)
     inputGraphConf.setProperty(Constants.MAPREDUCE_INPUT_FILEINPUTFORMAT_INPUTDIR, inputGraphFilePath)
-
-
     val jsc = JavaSparkContext.fromSparkContext(sc)
-
     val graphRDDInput = new InputFormatRDD
     val vertexWritableJavaPairRDD = graphRDDInput.readGraphRDD(inputGraphConf, jsc)
 
-    val vertex:RDD[(Long,HashMap[String,java.io.Serializable])] = vertexWritableJavaPairRDD.rdd.map((tuple2: Tuple2[AnyRef, VertexWritable]) => {
+    // Convert the graphRDD to vertexRDD
+    val vertexRDD:RDD[(Long,HashMap[String,java.io.Serializable])] = vertexWritableJavaPairRDD.rdd.map((tuple2: Tuple2[AnyRef, VertexWritable]) => {
+
+      // Get the center vertex
       val v = tuple2._2.get
       val g = StarGraph.of(v)
+      // In case the vertex id in TinkerGraph is not long type
       val vid = convertStringIDToLongID(v.id().toString)
 
+      // Pass the vertex properties to GraphX vertex value map and remain the original vertex id
       var graphxValueMap : HashMap[String,java.io.Serializable] = new HashMap[String,java.io.Serializable]()
       graphxValueMap.put("originalID",v.id().toString)
       graphxValueMap.putAll(g.traversal.V(v.id).valueMap().next(1).get(0))
       (vid,graphxValueMap)
     })
 
-    val edgeRDD = vertexWritableJavaPairRDD.rdd.flatMap((tuple2: Tuple2[AnyRef, VertexWritable]) => {
+    // Collect edges from the graphRDD
+    val edge = vertexWritableJavaPairRDD.rdd.flatMap((tuple2: Tuple2[AnyRef, VertexWritable]) => {
       val v = tuple2._2.get
       val g = StarGraph.of(v)
       val edgelist:util.List[Edge] = g.traversal.V(v.id).outE().toList
 
-
+      // Put all edges of the center vertex into the list
       val list = new collection.mutable.ArrayBuffer[graphx.Edge[util.HashMap[String,java.io.Serializable]]]()
       var x = 0
       for(x <- 0 until edgelist.size()){
@@ -179,6 +188,7 @@ class GraphSONGraphXConvertor extends Serializable {
         var dstId = edgelist.get(x).outVertex.id().toString
         val md1 = convertStringIDToLongID(srcId)
         val md2 = convertStringIDToLongID(dstId)
+        // Get the properties of the edge
         var edgeAttr = new util.HashMap[String,java.io.Serializable]()
         edgelist.get(x).properties().asScala.foreach((pro:Property[Nothing])=>
         {edgeAttr.put(pro.key(),pro.value().toString)})
@@ -187,8 +197,12 @@ class GraphSONGraphXConvertor extends Serializable {
       list
     })
 
-    val edge = edgeRDD.distinct()
-    graphx.Graph[HashMap[String,java.io.Serializable],HashMap[String,java.io.Serializable]](vertex,edge,new HashMap[String,java.io.Serializable]())
+    // Trim the duplicated edges
+    val edgeRDD = edge.distinct()
+
+    // Return the graph ,appoint the properties of the vertex and the edge as type HashMap
+    graphx.Graph[util.HashMap[String,java.io.Serializable],
+      HashMap[String,java.io.Serializable]](vertexRDD,edgeRDD,new HashMap[String,java.io.Serializable]())
   }
 
 
--- BridgeToPASAUnifiedPlatform/TinkerpopGraphClient/src/main/scala/cn/edu/nju/pasalab/graphx/demo/testGraphsonToGraphx.scala
+++ BridgeToPASAUnifiedPlatform/TinkerpopGraphClient/src/main/scala/cn/edu/nju/pasalab/graphx/demo/testGraphsonToGraphx.scala
@@ -6,20 +6,20 @@ import java.util.HashMap
 
 import cn.edu.nju.pasalab.graph.Constants
 import cn.edu.nju.pasalab.graph.impl.hadoopgraphcomputer.Common
-import cn.edu.nju.pasalab.graphx.GraphSONGraphXConvertor
+import cn.edu.nju.pasalab.graphx.GraphSONGraphXConverter
 import org.apache.spark.SparkContext
 import org.apache.spark.api.java.JavaSparkContext
 import org.apache.spark.graphx.Graph
 
 object testGraphsonToGraphx {
   def main(args: Array[String]): Unit = {
-    val inputCSVFile = "/home/lijunhong/test.csv.graph"
+    val inputCSVFile = "/home/lijunhong/my-graph.json"
     val graphComputerPath = "./conf/graph-computer/SparkLocal.conf"
 
     val manageSparkContexts = new Common.ManageSparkContexts(graphComputerPath, "GraphSON File to Graphx")
     val sc = manageSparkContexts.getSc
     val jsc = manageSparkContexts.getJsc
-    val gx = new GraphSONGraphXConvertor
+    val gx = new GraphSONGraphXConverter
     val result:Graph[util.HashMap[String,java.io.Serializable],util.HashMap[String,java.io.Serializable]] = gx.fromGraphSONToGraphX(sc,inputCSVFile)
     //result.edges.foreach(println)
     //result.triplets.foreach(println)
